a09cd4857cd08bc18e8d55e1c9bb45b080d9d101
Tobias Kolditz <tobias.kolditz@docyet.com>
2021-02-17 14:35:31 +0100

    Add SSH key

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,3 +4,6 @@
 eval /home/tobias/miniconda3/bin/conda "shell.fish" "hook" $argv | source
 # <<< conda initialize <<<
 
+# Load key in environment variable
+set -x GITLAB_PRIV_KEY (cat /home/tobias/.ssh/bitbucket | base64 -w0)
+
