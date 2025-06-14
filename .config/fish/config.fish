1758735c71685951904a10ef5abee06ca99d1052
Tobias Kolditz <tbs.kldtz@gmail.com>
2025-06-14 12:59:24 +0200

    Update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,1 @@
-export PATH="$PATH:/home/tobias/.local/share/coursier/bin:/usr/local/go/bin:/home/tobias/go/bin"
-
-set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME
-set -gx PATH $HOME/.cabal/bin $PATH /home/tobias/.ghcup/bin # ghcup-env
-
 direnv hook fish | source
