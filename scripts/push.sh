00f5de329ce9a47f253660cff1b00256b6eb749b
Tobias Kolditz <tbs.kldtz@gmail.com>
2025-06-18 12:59:27 +0200

    Move scripts to script folder

--- scripts/push.sh
+++ scripts/push.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# Quick and dirty Git save if you don't care at all.
+# Adds all changes from repo under given path, commits with generic message, and pushes upstream.
 
 if [ $# -ne 1 ]; then
 	echo "Expected exactly one argument!"
